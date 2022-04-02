46aceca89ba5877f7fde64c4a22201f1adb2243a
ebrahama <93477372+ebrahama@users.noreply.github.com>
2022-04-03 00:05:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const hum = document.querySelector(".hum");
+const close1 = document.querySelector(".close");
+const meno = document.querySelector(".menu");
+
+hum.addEventListener("click", () => {
+
+    const visi = meno.getAttribute('data-visible')
+
+    if(visi === "false"){
+
+        hum.setAttribute("data-visible" , false )
+        close1.setAttribute("data-visible" , true )
+        meno.setAttribute("data-visible" , true )
+
+    }
+})
+
+close1.addEventListener("click", () => {
+
+    const visi = meno.getAttribute('data-visible')
+
+    if(visi === "true"){
+
+        hum.setAttribute("data-visible" , true )
+        close1.setAttribute("data-visible" , false )
+        meno.setAttribute("data-visible" , false )
+
+    }
+})
